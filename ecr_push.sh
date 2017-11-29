62f9939b49464c330ac3cf7a80bdab1b0d1ca2ed
rstata <rstata@yahoo.com>
2017-11-29 01:27:14 -0800

    Fixing bugs

--- ecr_push.sh
+++ ecr_push.sh
@@ -4,7 +4,17 @@ if [ "$TRAVIS_BRANCH" != "rstata-wip" ]; then
   exit 0
 fi
 
-eval $(docker-compose -f cmds.yml run aws ecr get-login --region us-east-1)
+## Acrobatics here works around both awscli and MacOS problems I've
+## been encountering
+
+docker-compose -f cmds.yml run aws ecr get-login \
+                    --region us-east-1 --no-include-email > limbo-tmp
+
+cat limbo-tmp | sed 's/docker login -u AWS -p \([^ ]*\) .*/\1/' \
+ | docker login -u AWS --password-stdin \
+              560921689673.dkr.ecr.us-east-1.amazonaws.com
+
+rm limbo-tmp
 
 docker tag tim77/limbo:latest \
   560921689673.dkr.ecr.us-east-1.amazonaws.com/tim77-rstata/limbo:latest
