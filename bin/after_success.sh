9e530b15b9e665fde5452f486e9a30838b6aefe2
rstata <rstata@yahoo.com>
2017-11-30 01:03:11 -0800

    Initial step towards <STUDENT>-{wip|prod} naming convention

--- bin/after_success.sh
+++ bin/after_success.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
+export IS_WIP=`expr "$TRAVIS_BRANCH" : ".*-\(wip$\)"`
+export STUDENT=`expr "$TRAVIS_BRANCH" : "\(.*\)-[a-z]*$"`
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo Skipping after_success for pull request
   exit 0
 fi
 
-if [ "$TRAVIS_BRANCH" != "rstata-wip" ]; then
+if [ "$IS_WIP" != "wip" ]; then
   echo Skipping after_success for non-wip branch
   exit 0
 fi
 
+echo Student: $STUDENT
+
 openssl aes-256-cbc \
   -K $encrypted_887fc2baaf3c_key -iv $encrypted_887fc2baaf3c_iv \
-  -in secrets/rstata-env.sh.enc -out env.sh -d
+  -in secrets/${STUDENT}-env.sh.enc -out env.sh -d
 
 source secrets.sh
 
