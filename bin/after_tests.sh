edc915a93c85f0a6e96173fd8e7d23f965354b15
rstata <rstata@yahoo.com>
2017-12-01 00:02:12 -0800

    Swith to BOTNAME-wip/master convention

--- bin/after_tests.sh
+++ bin/after_tests.sh
@@ -1,25 +1,33 @@
 #!/bin/bash -ve
 
-export IS_WIP=`expr "$TRAVIS_BRANCH" : ".*-\(wip$\)"`
-export STUDENT=`expr "$TRAVIS_BRANCH" : "\(.*\)-[a-z]*$"`
-
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo Skipping after_success for pull request
+  echo Skipping after_test for pull request
   exit 0
 fi
 
-if [ "$IS_WIP" != "wip" ]; then
-  echo Skipping after_success for non-wip branch
+export IS_WIP=`expr "$TRAVIS_BRANCH" : ".*-\(wip$\)"`
+export IS_MASTER=`expr "$TRAVIS_BRANCH" : ".*-\(wip$\)"`
+export BOTNAME=`expr "$TRAVIS_BRANCH" : "\(.*\)-[a-z]*$"`
+
+if [ "$IS_WIP" = "wip" ]; then
+  export TYPE="wip"
+elif [ "$IS_MASTER" = "master" ]; then
+  export TYPE="master"
+else
+  echo Skipping after_test for non-wip/master branch
   exit 0
 fi
-
-echo Student: $STUDENT
 
 openssl aes-256-cbc \
   -K $encrypted_a8cf50fc24e7_key -iv $encrypted_a8cf50fc24e7_iv \
-  -in secrets/${STUDENT}-env.sh.enc -out secrets.sh -d
+  -in secrets/${BOTNAME}.sh.enc -out secrets.sh -d
 
 source ./secrets.sh
+if [ "$TYPE" = "master" ]; then
+  export SLACK_TOKEN=$MASTER_SLACK_TOKEN
+else
+  export SLACK_TOKEN=$WIP_SLACK_TOKEN
+fi
 
 bin/ecr_push.sh
 bin/ecs_deploy.sh $TRAVIS_BRANCH
