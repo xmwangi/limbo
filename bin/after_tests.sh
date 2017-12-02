aa8814df20dc39cb3ce1b4b9f941436a2f8fea7e
rstata <rstata@yahoo.com>
2017-12-02 00:03:50 -0800

    Modified so it better runs from laptop (goal: better simulate production push from laptop).

--- bin/after_tests.sh
+++ bin/after_tests.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -ve
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo Skipping after_test for pull request
-  exit 0
+# Designed to be used from one's laptop and from Travis.  The
+# existence of TRAVIS_BRANCH is used to test where it's running.
+#
+# In both cases, the standard AWS_ environment variables need to be
+# defined to provide the AWS credentials for accessing ECR and ECS.
+#
+# When running on a laptop, `git symbolic-ref --short HEAD` is used in
+# place of TRAVIS_BRANCH to determine what branch is being built.
+# Further, MASTER_SLACK_TOKEN and WIP_SLACK_TOKEN must be defined.
+
+
+if [ "$TRAVIS_BRANCH" != "" ]; then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo Skipping after_test for pull request
+    exit 0
+  fi
+  export THE_BRANCH=$TRAVIS_BRANCH
+else
+  export THE_BRANCH=`git symbolic-ref --short HEAD`
 fi
 
-export IS_WIP=`expr "$TRAVIS_BRANCH" : ".*-\(wip$\)"`
-export IS_MASTER=`expr "$TRAVIS_BRANCH" : ".*-\(master$\)"`
-export BOTNAME=`expr "$TRAVIS_BRANCH" : "\(.*\)-[a-z]*$"`
+export IS_WIP=`expr "$THE_BRANCH" : ".*-\(wip$\)"`
+export IS_MASTER=`expr "$THE_BRANCH" : ".*-\(master$\)"`
+export BOTNAME=`expr "$THE_BRANCH" : "\(.*\)-[a-z]*$"`
 
 if [ "$IS_WIP" = "wip" ]; then
   export TYPE="wip"
@@ -18,13 +34,14 @@ else
   exit 0
 fi
 
-openssl aes-256-cbc \
-  -K $encrypted_a8cf50fc24e7_key -iv $encrypted_a8cf50fc24e7_iv \
-  -in secrets/${BOTNAME}.sh.enc -out secrets.sh -d
-
-set +v # Don't reveal secrets to output log
-source ./secrets.sh
-set -v
+if [ "$MASTER_SLACK_TOKEN" = "" ] && [ "$WIP_SLACK_TOKEN" = "" ]; then
+  openssl aes-256-cbc \
+    -K $encrypted_a8cf50fc24e7_key -iv $encrypted_a8cf50fc24e7_iv \
+    -in secrets/${BOTNAME}.sh.enc -out secrets.sh -d
+  set +v # Don't reveal secrets to output log
+  source ./secrets.sh
+  set -v
+fi
 
 if [ "$TYPE" = "master" ]; then
   export SLACK_TOKEN=$MASTER_SLACK_TOKEN
@@ -39,4 +56,4 @@ fi
 export IMAGE_THIS_BUILD=$BOTNAME:$TYPE
 
 bin/ecr_push.sh
-bin/ecs_deploy.sh $TRAVIS_BRANCH
+bin/ecs_deploy.sh $THE_BRANCH
