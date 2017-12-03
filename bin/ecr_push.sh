a01f48b6ff7e7cb5fdf7f044688cecdcaf699a5e
rstata <rstata@yahoo.com>
2017-12-03 02:05:31 -0800

    Better comment.

--- bin/ecr_push.sh
+++ bin/ecr_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ve
 
-## Acrobatics here works around both awscli and MacOS problems I've
-## been encountering
+## The acrobatics here (ie, output to limbo-tmp, using sed to extract
+## password) are work-arounds to both awscli and MacOS problems.
 
 docker-compose -f cmds.yml run aws ecr get-login \
                     --region us-east-1 --no-include-email > limbo-tmp
