8a6b6c5637d55200f47468932dcb13d0a818f094
rstata <rstata@yahoo.com>
2017-12-16 18:55:47 -0800

    Initial implementation of CloudWatch metrics.

--- limbo/cloudwatchmetrics.py
+++ limbo/cloudwatchmetrics.py
@@ -1,6 +1,33 @@
+import boto3
+import re
+
 class CloudWatchMetrics(object):
+    VALID = "[^:&][^&]*(&[^&]*=[^&]*)*"
     def __init__(self, config):
+        cfg = config["cloudwatch"]
+        if not re.match(VALID, cfg):
+            e = ValueError("Bad CloudWatch configuration {0}.  Must match {1}."
+                           .format(cfg, VALID))
+            logger.error(str(e))
+            raise e
+        cfg = string.split(cfg, '&')
+        self._namespace = cfg[0]
+        self._dims = []
+        for dim in cfg[1:]:
+            nv = string.split(dim, '=')
+            self._dims.append({ 'Name': nv[0], 'Value': nv[1] })
+        self._client = boto3.client('cloudwatch')
         pass
 
     def events(self, count):
-        pass
+        self._client.put_metrics_data(
+            Namespace = self._namespace,
+            MetricData = [
+                {
+                    'MetricName': 'EventCount',
+                    'Dimensions': self._dims,
+                    'Value': 1,
+                    'Unit': 'Count'
+                }
+            ]
+        )
