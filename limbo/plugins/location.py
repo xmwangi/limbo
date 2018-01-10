d2a542537f6bd3fcde574321dee69dd9504bdb23
Xavier Mwangi <xmwangi@mit.edu>
2018-01-10 11:56:29 -0500

    Added simple pluging to get location of server running instance

---
+++ limbo/plugins/location.py
@@ -0,0 +1,17 @@
+"""!location returns city and country of the hosting server."""
+import re
+import requests
+
+def location():
+  response = requests.get("http://freegeoip.net/json").json()
+  return response['city'] + ', ' + response['country']
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"!location( .*)?", text)
+    if not match:
+        return
+
+    return location()
+
+on_bot_message = on_message
